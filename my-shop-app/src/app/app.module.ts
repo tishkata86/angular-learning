deb41c79dbd932aa3bed8130d5a063ec15f1937e
Tihomir Dobrev <tihomir.dobrev@a1.bg>
2023-03-16 18:43:43 +0200

    Add components

--- my-shop-app/src/app/app.module.ts
+++ my-shop-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecepiesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
